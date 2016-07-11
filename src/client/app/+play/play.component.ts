d553a00bd4670370215da8fc11f4d74ceb52f9ab
Phan Đình Lân <pdl.dev@outlook.com>
2016-07-10 19:32:12 -0700

    Create a grid using table based on query parameters Example: /play?N=5&P=6

--- src/client/app/+play/play.component.ts
+++ src/client/app/+play/play.component.ts
@@ -17,18 +17,43 @@ export class PlayComponent implements OnInit, OnDestroy{
   private content: number;
   private sub: any;
 
+  private rowCount: number;
+  private rowRange: any;
+  private columnCount: number;
+  private columnRange: any;
+
   constructor(
     private router: Router) {
     this.audioContext = new AudioContext();
+    
+    this.rowCount = 3;
+    this.rowRange = Array;
+    this.columnCount = 3;
+    this.columnRange = Array;
   }
 
   ngOnInit() {
+    var rowCount: number;
+    var columnCount: number;
+
     this.sub = this.router
       .routerState
       .queryParams
       .subscribe(params => {
         this.content = +params['content'];
+        rowCount = +params['N'];
+        columnCount = +params['P'];
       });
+
+    console.log("rowCount: " + rowCount);
+    if (isNaN(rowCount) !== true) {
+      this.rowCount = rowCount;
+    }
+
+    console.log("columnCount: " + columnCount);
+    if (isNaN(columnCount) !== true) {
+      this.columnCount = columnCount;
+    }
 
     console.log("Content: " + this.content.toString());
   }
